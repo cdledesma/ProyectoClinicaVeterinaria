88fdc8c924f20302d76c962b3f9d5a3302946980
Daniel <carlosdanielledesma76@gmail.com>
2024-05-26 16:43:36 +0200

    add footer

--- Js/app.js
+++ Js/app.js
@@ -1,5 +1,7 @@
-import {Publicidad} from "./publicidad.js";
+import { Publicidad } from "./publicidad.js";
+import { Footer } from "./footer.js";
 
-window.onload=function(){
-Publicidad();
+window.onload = function () {
+  Publicidad();
+  Footer();
 };
