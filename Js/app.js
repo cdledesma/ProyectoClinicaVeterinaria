4fb23ae2ff21627d1f9cff426b9b8edfb6ab9f8d
Daniel <carlosdanielledesma76@gmail.com>
2024-05-27 12:06:48 +0200

    actualizo archivo app.js

--- Js/app.js
+++
@@ -1,7 +0,0 @@
-import { Publicidad } from "./publicidad.js";
-import { Footer } from "./footer.js";
-
-window.onload = function () {
-  Publicidad();
-  Footer();
-};
---
+++ app.js
@@ -0,0 +1,7 @@
+import { Publicidad } from "./publicidad.js";
+import { Footer } from "./footer.js";
+
+window.onload = function () {
+  Publicidad();
+  Footer();
+};
